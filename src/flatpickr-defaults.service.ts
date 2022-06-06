a16cd6b2faad95c5c3a69956a182287d98003870
jorgeparavicini <j.m.paravicini@gmail.com>
2022-06-06 10:35:14 +0200

    update defaults

--- src/flatpickr-defaults.service.ts
+++ src/flatpickr-defaults.service.ts
@@ -335,7 +335,7 @@ export class FlatpickrDefaults implements FlatpickrDefaultsInterface {
   /**
    * HTML for the arrow icon, used to switch months.
    */
-  nextArrow: string = '>';
+  nextArrow: string = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M13.207 8.472l-7.854 7.854-0.707-0.707 7.146-7.146-7.146-7.148 0.707-0.707 7.854 7.854z' /></svg>";
 
   /**
    * Hides the day selection in calendar. Use it along with `enableTime` to create a time picker.
@@ -355,7 +355,7 @@ export class FlatpickrDefaults implements FlatpickrDefaultsInterface {
   /**
    * HTML for the left arrow icon.
    */
-  prevArrow: string = '<';
+  prevArrow: string = "<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 17 17'><g></g><path d='M5.207 8.471l7.146 7.147-0.707 0.707-7.853-7.854 7.854-7.853 0.707 0.707-7.147 7.146z' /></svg>";
 
   /**
    * Show the month using the shorthand version (ie, Sep instead of September).
